6eec50c36fc623415fb16428570ac36077ceff0a
Alexis Arce <arce.alexis@gmail.com>
2022-05-10 17:49:36 -0300

    Setup & Router

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <RouterLink to="/">Notes</RouterLink>
+  <RouterLink to="/stats">Stats</RouterLink>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
