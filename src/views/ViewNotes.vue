41ba62df3a984b59580ba9cf3aa4c01f4fdb5495
Alexis Arce <arce.alexis@gmail.com>
2022-05-12 13:40:14 -0300

    Add note functionality

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,32 +3,67 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note"></textarea>
+          <textarea
+            class="textarea"
+            v-model="newNote.content"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          ></textarea>
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            class="button is-link has-background-success"
+            @click="addNote"
+            :disabled="!newNote.content"
+          >
             Add New Note
           </button>
         </div>
       </div>
     </div>
-    <div v-for="i in 3" class="card mb-4">
+    <div v-for="note in notes" class="card mb-4">
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
-          explicabo consequuntur voluptate consectetur alias distinctio quis!
-          Dignissimos sed distinctio obcaecati excepturi natus, fuga aut
-          dolorem. Similique exercitationem accusamus odit soluta!
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
         <a href="#" class="card-footer-item">Edit</a>
         <a href="#" class="card-footer-item">Delete</a>
       </footer>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, reactive } from "vue";
+import { v4 as uuidv4 } from "uuid";
+
+const notes = reactive([
+  {
+    id: uuidv4(),
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam",
+  },
+  {
+    id: uuidv4(),
+    content: "This is a shorter note!",
+  },
+]);
+
+const newNote = reactive({});
+const newNoteRef = ref(null);
+
+const addNote = () => {
+  notes.unshift({
+    id: uuidv4(),
+    content: newNote.content,
+  });
+
+  newNote.content = "";
+  newNoteRef.value.focus();
+};
+</script>
